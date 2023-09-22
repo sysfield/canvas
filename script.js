4e43eedaf66ad3058509f795dd69f9191c5d0212
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-23 00:36:08 +0100

    Capture slider value to send to canvasSize function

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ const container = document.getElementById('container');
 
 const sizeSlider = document.getElementById('sizeSlider');
 
-console.log(sizeSlider.valueAsNumber);
+// console.log(sizeSlider.valueAsNumber);
+
+sizeSlider.addEventListener('mouseup', canvasSize(sizeSlider.valueAsNumber))
+
+// canvasSize(sizeSlider.valueAsNumber)
+
+function parseNumber(number)
+{
+    switch (number)
+    {
+        case 0:
+            return 16;
+        case 33:
+            return 32;
+        case 66:
+            return 64;
+        case 99:
+            return 128;
+    }
+}
 
 function canvasSize(size)
 {
+
+    size = parseNumber(size);
+
     for (var i = 0; i < size; i++)
     {
         container.innerHTML += `<div class="pixel" id="pixel_${i}"></div>`;
