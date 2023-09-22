17fd3b5b81295e5959424ece19d693343a3ea847
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-22 22:34:49 +0100

    Fix for loop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.getElementById('container');
 
-for (var i = 0; i < 15; i++)
+for (var i = 0; i < 16; i++)
 {
     container.innerHTML += `<div class="pixel" id="pixel_${i}"></div>`;
 }
